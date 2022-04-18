6cf6d7447a6539f6b66910845487cbd4083fece6
ychiguer <chiguer.yo@gmail.com>
2022-04-18 11:02:47 +0200

    Squashed commit of the following:
    commit 8f8688dc17c32fa235789c6c5d7042d13799546f
    Merge: bdfa2ff8 4892dcec
    Author: Youssef Chiguer <80593002+ychiguer@users.noreply.github.com>
    Date:   Mon Apr 18 10:55:31 2022 +0200
    
        Merge pull request #7 from ychiguer/DEV-12-modules-missing-of-main-branch
    
        Modules Upload
    
    commit 4892dcec3fb2f209bec1545ee9a0aecacbc7603e
    Author: ychiguer <chiguer.yo@gmail.com>
    Date:   Mon Apr 18 10:52:49 2022 +0200
    
        Modules Upload
    
    commit bdfa2ff8aa4591e2d43cf542a1527d6f3d974bf7
    Merge: 913f52ea b5371de8
    Author: Youssef Chiguer <80593002+ychiguer@users.noreply.github.com>
    Date:   Mon Apr 18 10:48:53 2022 +0200
    
        Merge pull request #6 from ychiguer/DEV-11-react-architecture
    
        Dev 11 react architecture
    
    commit b5371de8a997932cd7a355a82889cee104efde30
    Author: ychiguer <chiguer.yo@gmail.com>
    Date:   Mon Apr 18 10:47:01 2022 +0200
    
        First Setup
    
    commit dc28a069a37aa059af6c56e00115bb5a69f41c20
    Author: ychiguer <chiguer.yo@gmail.com>
    Date:   Wed Apr 13 20:54:38 2022 +0200
    
        Update boilerplate
    
    commit 913f52ea447b070e205c7d4bd787a26196474dc3
    Merge: 80c5cb28 d1ce2529
    Author: aminaelkh <79382106+aminaelkh@users.noreply.github.com>
    Date:   Wed Apr 13 15:05:29 2022 +0200
    
        Merge pull request #4 from ychiguer/DEV-8-select-a-color-theme
    
        Merge pull request #3 from ychiguer/main

---
+++ render/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
