9d0a6b7464d6b5740c51b14e7a629f77553a5e03
Hamza Ennaoui <83406845+HamzaEnn@users.noreply.github.com>
2022-05-13 09:11:31 +0200

    Add files via upload

--- render/src/App.js
+++ render/src/App.js
@@ -3,13 +3,19 @@ import { BrowserRouter as Router, Routes, Route }
   from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import SignUp from './pages/signup';
+import About from './pages/about';
+import Navigation from './pages/navigation';
 
 function App() {
   return (
     <Router>
+    <Navigation/>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
     </Router>
   );
