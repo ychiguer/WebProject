6b3325ed270fab58e14be8744e35272db9eb60bc
Hamza Ennaoui <hamza.ennaoui@outlook.fr>
2022-05-13 08:37:36 +0200

    Revert "Add files via upload"
    This reverts commit 23fb1063d1ce1e8c4fb7d3cce9e55d0c61176e6a.

--- render/src/App.js
+++ render/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route }
   from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
-import SignUp from './pages/signup';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
-        <Route exact path='/signup' element={<SignUp />} />
       </Routes>
     </Router>
   );
