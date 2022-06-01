601031a6957c8a1762ef5c35b7bd269c8bcc849a
Hamza Ennaoui <hamza.ennaoui@outlook.fr>
2022-06-02 00:42:32 +0200

    login

--- render/src/pages/login.js
+++ render/src/pages/login.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import '../css/login.css';
+import { NavLink } from "react-router-dom";
 
 function Login() {
   return (
-    <div className="App">
-      <h1>Login Page TODO</h1>
+    <div class="container">
+    <div class="child">
+  <form id="form_signup">
+    <h3>Login</h3>
+
+    <div className="mb-3">
+      <label>Email
+      <input
+        type="email"
+        className="form-control"
+        placeholder="Enter email"
+      /></label>
     </div>
+    <div className="mb-3">
+      <label>Password
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+      /></label>
+    </div>
+    <div className="d-grid">
+      <button type="submit" className="btn btn-primary">
+        Login
+      </button>
+    </div>
+    <p className="forgot-password text-right">
+      <NavLink to="/signup"> SignUp </NavLink>
+    </p>
+  </form>
+  </div></div>
   );
 }
 
